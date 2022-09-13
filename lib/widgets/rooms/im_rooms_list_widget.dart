d92ae38a6a87d6d6ffcc280f2f910042cfc78632
11 <iMill1611@gmail.com>
2022-09-13 21:41:37 +0800

    remove loading

--- lib/widgets/rooms/im_rooms_list_widget.dart
+++ lib/widgets/rooms/im_rooms_list_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:imkit/gen/assets.gen.dart';
 import 'package:imkit/models/im_room.dart';
 import 'package:imkit/sdk/imkit.dart';
-import 'package:imkit/utils/im_loading.dart';
 import 'package:imkit/widgets/components/im_empty_widget.dart';
 import 'package:imkit/widgets/rooms/im_rooms_list_item.dart';
 
@@ -16,25 +15,15 @@ class IMRoomsListWidget extends StatelessWidget {
         stream: IMKit.instance.listener.watchRooms,
         builder: (BuildContext context, AsyncSnapshot<List<IMRoom>> snapshot) {
           ///获取到数据，为所欲为的更新 UI
-          print(">>> room list count: ${snapshot.data?.length ?? 0}");
-          // inspect(snapshot.data);
-          //return Container();
+          debugPrint(">>> room list count: ${snapshot.data?.length ?? 0}");
           final itemCount = snapshot.data?.length ?? 0;
           if (itemCount <= 0 && IMKit.uid.isNotEmpty) {
             return emptyView ?? IMEmptyWidget(icon: Assets.images.emptyRooms, message: IMKit.S.rooms_empty);
           } else {
-            return Stack(
-              children: [
-                ListView.separated(
-                    itemBuilder: (BuildContext context, int index) => IMRoomsListItem(key: ObjectKey(snapshot.data![index].id), room: snapshot.data![index]),
-                    separatorBuilder: (BuildContext context, int index) => const Divider(height: 1),
-                    itemCount: IMKit.uid.isNotEmpty ? itemCount : 0),
-                Visibility(
-                  visible: snapshot.connectionState == ConnectionState.waiting,
-                  child: IMLoading.view,
-                ),
-              ],
-            );
+            return ListView.separated(
+                itemBuilder: (BuildContext context, int index) => IMRoomsListItem(key: ObjectKey(snapshot.data![index].id), room: snapshot.data![index]),
+                separatorBuilder: (BuildContext context, int index) => const Divider(height: 1),
+                itemCount: IMKit.uid.isNotEmpty ? itemCount : 0);
           }
         },
       );
