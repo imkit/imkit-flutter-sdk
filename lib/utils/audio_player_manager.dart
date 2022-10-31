ae93d3c096e64b6479a79472a2dee910804f9774
11 <iMill1611@gmail.com>
2022-10-31 22:05:40 +0800

    fix play audio volume

--- lib/utils/audio_player_manager.dart
+++ lib/utils/audio_player_manager.dart
@@ -2,8 +2,28 @@ import 'package:audioplayers/audioplayers.dart' as ap;
 
 class AudioPlayerManager {
   static final AudioPlayerManager instance = AudioPlayerManager._();
+  final ap.AudioContext _audioContext = ap.AudioContext(
+    iOS: ap.AudioContextIOS(
+      defaultToSpeaker: true,
+      category: ap.AVAudioSessionCategory.ambient,
+      options: [
+        ap.AVAudioSessionOptions.defaultToSpeaker,
+        ap.AVAudioSessionOptions.mixWithOthers,
+      ],
+    ),
+    android: ap.AudioContextAndroid(
+      isSpeakerphoneOn: true,
+      stayAwake: true,
+      contentType: ap.AndroidContentType.sonification,
+      usageType: ap.AndroidUsageType.assistanceSonification,
+      audioFocus: ap.AndroidAudioFocus.none,
+    ),
+  );
+
   ap.AudioPlayer? _audioPlayer;
-  AudioPlayerManager._();
+  AudioPlayerManager._() {
+    ap.AudioPlayer.global.setGlobalAudioContext(_audioContext);
+  }
 
   void play({required ap.AudioPlayer player, required ap.Source source}) async {
     if (_audioPlayer != null) {
