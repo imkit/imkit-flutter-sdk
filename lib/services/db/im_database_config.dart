9a348bf7ed08d596a45096b53f89ec2fced41e48
11 <iMill1611@gmail.com>
2022-11-12 00:47:18 +0800

    database migration for version 2

--- lib/services/db/im_database_config.dart
+++ lib/services/db/im_database_config.dart
@@ -4,6 +4,10 @@ class IMDatabaseConfig {
   static const version = 2;
 
   static List<Migration> migrations() {
-    return [];
+    return [
+      Migration(1, 2, (database) async {
+        await database.execute('CREATE TABLE IF NOT EXISTS `IMMessageMark` (`id` TEXT NOT NULL, `isDelete` INTEGER NOT NULL, PRIMARY KEY (`id`))');
+      }),
+    ];
   }
 }
